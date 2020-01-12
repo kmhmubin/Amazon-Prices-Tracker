62e9dc18a0b510cc97566f899745c1f798c5f94b
K.M.H.Mubin <52270073+kmhmubin@users.noreply.github.com>
2020-01-12 21:49:13 +0600

    rewrite the code

--- amazon price tracker/scraper.py
+++ amazon price tracker/scraper.py
@@ -1,65 +1,47 @@
 # import packages
-
+import re
 import requests
 from bs4 import BeautifulSoup
 
-
-# URL verification function
-def url_verifier(url):
-    if url.find("www.amazon.com") != -1:
-        index = url.find("/dp/")
-        if index != -1:
-            index2 = index + 14
-            url = "https://wwww.amazon.com" + url[index:index2]
-        else:
-            index = url.find("/gp/")
-            if index != -1:
-                index2 = index + 22
-                url = "https://www.amazon.com" + url[index:index2]
-            else:
-                url = None
-    else:
-        url = None
-    return url
+# amazon product url link
+url = "https://www.amazon.com/Acer-Display-Graphics-Keyboard-A515-43-R19L/dp/B07RF1XD36/ref=sr_1_1?fst=as%3Aoff&qid=1578819515&rnid=16225007011&s=computers-intl-ship&sr=1-1"
 
 
 # Price converter
 def converted_price(price):
-    stripped_price = price.strip("$ ,")
-    replaced_price = stripped_price.replace(",", "")
-    find_dot = replaced_price.find(".")
-    to_convert_price = replaced_price[0:find_dot]
-    convert_price = int(to_convert_price)
-
+    convert_price = float(re.sub(r"[^\d.]", "", price))  # converting the string into float
     return convert_price
 
 
 # Scarping product details
-def products_details(url):
+def products_details():
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36"}
 
-    details = {"name": "", "price": 0, "deal": True, "url": ""}
-    _url = url_verifier(url)
-    if _url == "":
-        details = None
-    else:
+    details = {"name": "",
+               "price": 0,
+               "deal": True,
+               "url": ""}
+    _url = url  # calling the url link
+    if _url == "":  # checking the url is empty or not
+        details = None  # if url empty it will print None
+    else:  # if the url not empty
         page = requests.get(url, headers=headers)
         soup = BeautifulSoup(page.content, "html5lib")
-        title = soup.find(id="productTitle")
-        price = soup.find(id="priceblock_dealprice")
-        if price is None:
-            price = soup.find(id="priceblock_ourprice")
-            details["deal"] = False
-        if title is not None and price is not None:
-            details["name"] = title.get_text().strip()
-            details["price"] = converted_price(price.get_text())
-            details["url"] = _url
+        title = soup.find(id="productTitle")  # it will find the product title
+        price = soup.find(id="priceblock_dealprice")  # it will find the deal price
+        if price is None:  # if there is no deal price
+            price = soup.find(id="priceblock_ourprice")  # it will check amazon price
+            details["deal"] = False  # if there is no deal price it will print false
+        if title is not None and price is not None:  # if the title and price is valid
+            details["name"] = title.get_text().strip()  # it will extract the title name and print
+            details["price"] = converted_price(
+                price.get_text())  # calling the convert price function and print the price
+            details["url"] = _url  # print the url
         else:
-            return None
+            return None  # if nothing found it will print None
     return details
 
 
-# printing the product details
-print(products_details(
-    "https://www.amazon.com/Acer-Display-Graphics-Keyboard-A515-43-R19L/dp/B07RF1XD36/ref=sr_1_1?fst=as%3Aoff&qid=1578819515&rnid=16225007011&s=computers-intl-ship&sr=1-1"))
+# run the function and print the details
+print(products_details())
