ef3fe63463cc0fba85520b8c9ec64bed8e99aa44
K.M.H.Mubin <52270073+kmhmubin@users.noreply.github.com>
2020-01-16 21:56:08 +0600

    minor bug fix

--- amazon price tracker/scraper.py
+++ amazon price tracker/scraper.py
@@ -1,51 +1,72 @@
 # import packages
-import re
+import re  # a build-in regular expresion module
 import requests
 from bs4 import BeautifulSoup
 
-# amazon product url link
-url = "https://www.amazon.com/Acer-Display-Graphics-Keyboard-A515-43-R19L/dp/B07RF1XD36/ref=sr_1_1?fst=as%3Aoff&qid=1578819515&rnid=16225007011&s=computers-intl-ship&sr=1-1"
+# Storing the link in a variable
+URL = "https://www.amazon.com/Acer-Display-Graphics-Keyboard-A515-43-R19L/dp/B07RF1XD36/ref=sr_1_1?fst=as%3Aoff&qid=1578819515&rnid=16225007011&s=computers-intl-ship&sr=1-1"
 
 
-# Price converter
+# price converter function
 def converted_price(price):
-    convert_price = float(re.sub(r"[^\d.]", "", price))  # converting the html elements into float
+    # convert html elements into float
+    convert_price = float(re.sub(r"[^\d.]", "", price))
     return convert_price
 
 
-# Scarping product details
-def products_details():
-    # containing the user-agent
+# Extract product details function
+def product_details():
+    # add user-agent
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36"}
-    # storing product details  in a dictionary
-    details = {"name": "",
-               "price": 0,
-               "deal": True,
-               "availability": "",
-               "url": ""}
-    _url = url  # calling the url variable
-    if _url == "":  # checking the url is empty or not
-        details = None  # if url empty it will print None
-    else:  # if the url not empty
-        page = requests.get(url, headers=headers)
+
+    # storing product details in a dictionary
+    details = {
+        "Name": "",
+        "Price": 0,
+        "Deal": True,
+        "Availability": "",
+        "Product URL": ""
+    }
+
+    # Calling the url variable
+    _url = URL
+    # checking the url is empty or not
+    if _url == "":
+        # if url empty it will print none
+        details = None
+    # if url not empty
+    else:
+        page = requests.get(URL, headers=headers)
         soup = BeautifulSoup(page.content, "html5lib")
-        title = soup.find(id="productTitle")  # it will find the product title
-        price = soup.find(id="priceblock_dealprice")  # it will find the deal price
-        stock = soup.find(id="availability")  # it will check the product is available or not
-        if price is None:  # if there is no deal price
-            price = soup.find(id="priceblock_ourprice")  # it will check amazon price
-            details["deal"] = False  # if there is no deal price it will print false
-        if title is not None and price is not None:  # if the title and price is valid
-            details["name"] = title.get_text().strip()  # it will extract the title name and print
-            details["price"] = converted_price(
+        # it will find the product title
+        title = soup.find(id="productTitle")
+        # it will find the deal price
+        price = soup.find(id="priceblock_dealprice")
+        # it will check the product is available or not
+        stock = soup.find(id="availability")
+        # it will check the product is available or not
+        if price is None:
+            # if there is no deal price
+            # it will check amazon price
+            price = soup.find(id="priceblock_ourprice")
+            details["Deal"] = False  # if no deal it will print False
+
+        # if the title and price is valid
+        if title is not None and price is not None:
+            # it will extract the title name and print
+            details["Name"] = title.get_text().strip()
+            details["Price"] = converted_price(
                 price.get_text())  # calling the convert price function and print the price
-            details["availability"] = stock.get_text().strip()  # it will find the availability of the product and print
-            details["url"] = _url  # print the url
+            # it will find the availability of the product and print
+            details["Availability"] = stock.get_text().strip()
+            details["Product URL"] = _url  # print the product url
+        # if nothing found it will print None
         else:
-            return None  # if nothing found it will print None
+            return None
+
     return details
 
 
 # run the function and print the details
-print(products_details())
+print(product_details())
